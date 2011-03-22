e3319afc3294a48cbab5b7456bc7a9e0da6b5ee6
leejohn1987 <leejohn1987@fd3c630b-2fd9-63d7-6e11-da820b025433>
2011-03-22 05:33:05 +0000

    1. continue doing EventAdd

--- trunk/Event Assistance/src/ustc/sse/assistant/MainScreen.java
+++ trunk/Event Assistance/src/ustc/sse/assistant/MainScreen.java
@@ -7,6 +7,8 @@ package ustc.sse.assistant;
 
 import ustc.sse.assistant.contact.ContactList;
 import ustc.sse.assistant.event.EventAdd;
+import ustc.sse.assistant.help.HelpActivity;
+import ustc.sse.assistant.setting.Setting;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -42,6 +44,25 @@ public class MainScreen extends Activity {
 			public void onClick(View v) {
 				Intent eventIntent = new Intent(MainScreen.this, EventAdd.class);
 				MainScreen.this.startActivity(eventIntent);
+			}
+		});
+    	
+    	ImageButton settingButton = (ImageButton) findViewById(R.id.setting_imageButtojn);
+    	settingButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent settingIntent = new Intent(MainScreen.this, Setting.class);
+				startActivity(settingIntent);
+			}
+		});
+    	ImageButton helpButton = (ImageButton) findViewById(R.id.help_imageButton);
+    	helpButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent helpIntent = new Intent(MainScreen.this, HelpActivity.class);
+				startActivity(helpIntent);
 			}
 		});
     	
---
+++ trunk/Event Assistance/src/ustc/sse/assistant/event/EventConstant.java
@@ -0,0 +1,7 @@
+package ustc.sse.assistant.event;
+
+public interface EventConstant {
+	int EVENT_ALARM_TYPE_RING = 0;
+	int EVENT_ALARM_TYPE_VIBRE = 1;
+	int EVENT_ALARM_TYPE_NOTIFICATION = 2;
+}
