8e9be31d3d6288388846abe15691536f01c45bfb
leejohn1987 <leejohn1987@fd3c630b-2fd9-63d7-6e11-da820b025433>
2011-03-27 14:38:41 +0000

    event list finished remains delete button

--- trunk/Event Assistance/src/ustc/sse/assistant/event/EventList.java
+++ trunk/Event Assistance/src/ustc/sse/assistant/event/EventList.java
@@ -3,8 +3,12 @@
  */
 package ustc.sse.assistant.event;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import ustc.sse.assistant.R;
 import ustc.sse.assistant.event.provider.EventAssistant.Event;
@@ -13,10 +17,14 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
@@ -25,7 +33,6 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.ResourceCursorAdapter;
 import android.widget.TextView;
-import android.widget.Toast;
 
 /**
  * a event list 
@@ -39,6 +46,8 @@ public class EventList extends Activity {
 	
 	public static final String DATE_FORMAT = "yyyy年MM月dd日";
 	
+	private static final int HEADER_FOOTER_NUMBER = 1;
+	
 	private ListView listView;
 	private Button selectButton;
 	private Button deleteButton;
@@ -47,18 +56,64 @@ public class EventList extends Activity {
 	private Calendar fromCalendar;
 	private Calendar toCalendar;
 	
+	private Set<Long> selectedItemIds = new HashSet<Long>();
+	private List<ImageView> imageViewList = new ArrayList<ImageView>();
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.event_list);
 		
-		initiateWidgets();
+		getWidgets();
 		initiateEvents();
 		initiateBirthdayInfo();
+		initiateButtons();
 	}
 
 
-	private void initiateWidgets() {
+	private void initiateButtons() {
+		selectButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				int count = listView.getAdapter().getCount() - HEADER_FOOTER_NUMBER;
+				int size = selectedItemIds.size();
+				//all items selected
+				if ( size < count) {
+					selectButton.setText(getString(R.string.event_list_deselect_button));
+					for (ImageView iv : imageViewList) {
+						iv.setSelected(true);
+						selectedItemIds.add((Long) iv.getTag());
+					}
+					
+				} else if (size >= count){
+					//when all items was selected, then deselect all items
+					selectButton.setText(getString(R.string.event_list_select_button));
+					for (ImageView iv : imageViewList) {
+						iv.setSelected(false);
+						selectedItemIds.remove((Long) iv.getTag());
+						
+					}
+					buttonBar.setVisibility(View.INVISIBLE);
+				}
+				
+				deleteButton.setText(getString(R.string.event_list_delete_button) + "(" + selectedItemIds.size() + ")");
+			}
+		});
+		
+		deleteButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+	}
+
+
+	private void getWidgets() {
 		listView = (ListView) findViewById(R.id.event_list_listView);
 		selectButton = (Button) findViewById(R.id.event_list_select_button);
 		deleteButton = (Button) findViewById(R.id.event_list_delete_button);
@@ -71,8 +126,8 @@ public class EventList extends Activity {
 		public void onItemClick(AdapterView<?> parent, View view, int position,
 				long id) {
 			Intent i = new Intent(EventList.this, EventDetail.class);
-			Toast.makeText(EventList.this, String.valueOf(id), Toast.LENGTH_SHORT).show();
-			
+			//Toast.makeText(EventList.this, String.valueOf(id), Toast.LENGTH_SHORT).show();
+			//TODO start event detail
 		}
 	};
 	
@@ -101,11 +156,20 @@ public class EventList extends Activity {
 		}
 		ContentResolver cr = getContentResolver();
 		String[] projection = {Event._ID, Event.CONTENT, Event.LOCATION, Event.BEGIN_TIME, Event.END_TIME};
-		String selection = Event.BEGIN_TIME + " > ? AND " + Event.END_TIME + " < ? ";
+		String selection = Event.BEGIN_TIME + " >=  ? AND " + Event.BEGIN_TIME + " <= ? ";
 		String[] selectionArgs = {String.valueOf(fromCalendar.getTimeInMillis()),
 									String.valueOf(toCalendar.getTimeInMillis())};
 		Cursor cursor = cr.query(Event.CONTENT_URI, projection, selection, selectionArgs, null);
-		EventListCursorAdapter adapter = new EventListCursorAdapter(this, R.layout.event_list_item, cursor);
+		EventListCursorAdapter adapter = new EventListCursorAdapter(this, 
+														R.layout.event_list_item, 
+														cursor,
+														imageViewListener,
+														imageViewList);
+		
+		TextView tv = new TextView(this);
+		tv.setLayoutParams(new AbsListView.LayoutParams(LayoutParams.FILL_PARENT, 50));
+		tv.setText("查看更多事件");
+		listView.addFooterView(tv);	
 		
 		listView.setAdapter(adapter);
 		listView.setOnItemClickListener(listViewItemListener);
@@ -118,24 +182,63 @@ public class EventList extends Activity {
 		
 	}
 	
+	//this listener is used by the check image view only
+	private OnClickListener imageViewListener = new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			int count = listView.getAdapter().getCount() - HEADER_FOOTER_NUMBER;
+			int size = selectedItemIds.size();
+
+			//Toast.makeText(EventList.this, v.toString() + " " + v.getTag() + " " + v.isSelected(), Toast.LENGTH_SHORT).show();
+			if (v.isSelected()) {
+				if (size == count) {
+					selectButton.setText(getString(R.string.event_list_select_button));
+				}
+				//if size equal 1, here we hide the button bar
+				if (size == 1) {
+					buttonBar.setVisibility(View.INVISIBLE);
+				}
+				v.setSelected(false);
+				selectedItemIds.remove(v.getTag());
+			} else {
+				
+				if (size + 1 == count) {
+					selectButton.setText(getString(R.string.event_list_deselect_button));
+				}
+				
+				v.setSelected(true);
+				selectedItemIds.add((Long) v.getTag());
+								
+				//show the button bar
+				if (size == 0) {
+					buttonBar.setVisibility(View.VISIBLE);
+				}
+			}
+			
+			deleteButton.setText(getString(R.string.event_list_delete_button) + "(" + selectedItemIds.size() + ")");
+			
+		}
+	
+	};
+
 	private static class EventListCursorAdapter extends ResourceCursorAdapter {
 		private Context context;
-		
-		private OnClickListener checkListener = new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Toast.makeText(context, "ImageView clicked" + v.getTag(), Toast.LENGTH_SHORT).show();
-				v.setSelected(v.isSelected() ? false : true);			
-				v.refreshDrawableState();
-				
-				//TODO add or remove corresponding event id in the set
-			}
-		};
+		private OnClickListener imageViewListener;
+		private List<ImageView> imageViews;
 
-		public EventListCursorAdapter(Context context, int layout, Cursor c) {
+		public EventListCursorAdapter(Context context, int layout, Cursor c, OnClickListener l, List<ImageView> imageViews) {
 			super(context, layout, c);
 			this.context = context;
+			this.imageViewListener = l;
+			this.imageViews = imageViews;
+			
+		}
+		
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			return super.getView(position, null, parent);
+		     
 		}
 
 		@Override
@@ -151,18 +254,22 @@ public class EventList extends Activity {
 			String beginTimeStr = cursor.getString(cursor.getColumnIndex(Event.BEGIN_TIME));
 			String endTimeStr = cursor.getString(cursor.getColumnIndex(Event.END_TIME));
 			
-			checkImageView.setOnClickListener(checkListener);
-			checkImageView.setTag(id);
+			checkImageView.setOnClickListener(imageViewListener);
+			Drawable drawable = context.getApplicationContext().getResources().getDrawable(R.drawable.indicator_check_mark_light);
+			checkImageView.setImageDrawable(drawable.mutate());
+			checkImageView.setTag(id); 
+			imageViews.add(checkImageView);
 			
 			Calendar begin = Calendar.getInstance();
 			begin.setTimeInMillis(Long.valueOf(beginTimeStr));
 			Calendar end = Calendar.getInstance();
-			end.setTimeInMillis(Long.valueOf(endTimeStr));
+			end.setTimeInMillis(Long.valueOf(endTimeStr));		
 			
 			dateTv.setText("From " + DateFormat.format(DATE_FORMAT, begin) 
 							+ " to " + DateFormat.format(DATE_FORMAT, end));
 			contentTv.setText(content);
 			locationTv.setText(location);
 		}
+
 	}
 }
