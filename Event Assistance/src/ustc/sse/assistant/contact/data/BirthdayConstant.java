0d8ccb8e92f6d942fd387e43ce31c468ccbcfbec
leejohn1987 <leejohn1987@fd3c630b-2fd9-63d7-6e11-da820b025433>
2011-03-30 14:24:11 +0000

    修改生日的储存方式，分为 年，月，日 格式为公历

---
+++ Event Assistance/src/ustc/sse/assistant/contact/data/BirthdayConstant.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package ustc.sse.assistant.contact.data;
+
+/**
+ * @author 李健
+ *
+ */
+public interface BirthdayConstant {
+	
+	String TYPE = "ustc.sse.EventAssitant.birthday";
+	String YEAR = "data1";
+	String MONTH = "data2";
+	String DAY = "data3";
+	
+}
